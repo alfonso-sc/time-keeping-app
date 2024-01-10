94ab9a69c9b8f8142ab0e48489b0c36135cf070c
scuser <scuser@MacBook-Pro-1381.local>
2024-01-10 16:01:04 -0700

    first pass at user details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
