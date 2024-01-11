4e9fd4997fde77552870a32cd5e622a407148be1
alfonso <alfonso@steeleconsult.com>
2024-01-10 21:33:15 -0700

    create message service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { USERS } from '../../mock-users';
 import { User } from '../../interfaces/user';
+import { MessageService } from '../message/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getUsers(): User[] {
+    this.messageService.add('UserService: fetched users');
     return USERS;
   }
 }
