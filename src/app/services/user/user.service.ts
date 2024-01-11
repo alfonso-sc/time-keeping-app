1dd73f4a07877faf8109dd68d84f2a4541db63b2
alfonso <alfonso@steeleconsult.com>
2024-01-10 21:58:22 -0700

    make user service async and use

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { USERS } from '../../mock-users';
 import { User } from '../../interfaces/user';
 import { MessageService } from '../message/message.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,9 @@ export class UserService {
 
   constructor(private messageService: MessageService) { }
 
-  getUsers(): User[] {
+  getUsers(): Observable<User[]> {
     this.messageService.add('UserService: fetched users');
-    return USERS;
+    const users = of(USERS);
+    return users;
   }
 }
